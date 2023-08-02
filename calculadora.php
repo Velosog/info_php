4f3bfeb8f2f2966c725f1b7262a9d71608f24717
Velosog <gabrielpveloso@gmail.com>
2023-08-01 21:33:14 -0300

    aula 01/08 raizes e porcentagem

--- calculadora.php
+++ calculadora.php
@@ -1,11 +1,11 @@
 <?php
- $Value = "2";
- $Value2 = "4";
- $Operador = "%";
+ $Value = "10";
+ $Value2 = "16";
+ $Operador = "ma";
  $Resultado = 0;
  $NúmeroAlto = 40;
 
-  if ($Operador == "+" ) {
+  if ($Operador == "+") {
     $Resultado = $Value + $Value2;
   }
 
@@ -21,11 +21,11 @@
     $Resultado = $Value * $Value2;
   }
 
-  if ($Resultado >= $NúmeroAlto ) {
+  if ($Resultado >= $NúmeroAlto) {
     echo "É um número alto <br> ";
   }
   else {
-    echo "Não é um número alto <br>";
+    echo "Não é um número alto <br>"; //"Não é um número alto
   }
 
   if ($Operador == "**") {
@@ -34,12 +34,57 @@
 
   if ($Operador == "%") {
     $resto = $Value % $Value2;
-    if ($resto == 0){
-      $Resultado = "O número é par";  
+    if ($resto == 0) {
+      echo $Resultado == "O número é par";  
     }  else {
-      $Resultado = "O número é par";
+      echo $Resultado == "O número é impar";
     }
   }
  
-  echo "O valor é {$Resultado}";
+  if ($Resultado >= 1) {
+    $Resultado = "O número é positivo <br>";
+  } else if ($Resultado <= -1) {
+    $Resultado = "O número é negativo";
+  } else "O número é nulo"; 
  
+  if ($Operador == "perc") {
+    $Resultado = $Value - ($Value * $Value2) / 100; 
+  }
+ 
+  if ($Operador == "perc+") {
+    $Resultado = $Value + ($Value * $Value2) / 100; 
+  }
+
+  if ($Operador == "perc*") {
+    $Resultado = $Value * ($Value * $Value2) / 100; 
+  }
+
+  if ($Operador == "perc/") {
+    $Resultado = $Value / ($Value * $Value2) / 100; 
+  }
+
+  if ($Operador == "sqrt") {
+    $Resultado = sqrt($Value); 
+  }
+
+  if ($Operador == "+sqrt") {
+    $Resultado = sqrt($Value) + sqrt($Value2);
+  }
+
+  if ($Operador == "-sqrt") {
+    $Resultado = sqrt($Value) - sqrt($Value2);
+  }
+ 
+  if ($Operador == "/sqrt") {
+    $Resultado = sqrt($Value) / sqrt($Value2);
+  }
+
+  if ($Operador == "*sqrt") {
+    $Resultado = sqrt($Value) * sqrt($Value2);
+  }
+
+  if ($Operador == "ma") {
+    $Resultado = ($Value + $Value2) / 2;
+  }
+
+  echo "O valor é {$Resultado}";
