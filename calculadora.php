550b4808b67ee3a05daebce732127776de4c04aa
Velsog <gabrielpveloso@gmail.com>
2023-08-08 20:57:44 -0300

    Repetidores

--- calculadora.php
+++ calculadora.php
@@ -87,4 +87,36 @@
     $Resultado = ($Value + $Value2) / 2;
   }
 
-  echo "O valor é {$Resultado}";
+  echo "O valor é {$Resultado} <br>";
+
+  $c= 5;
+
+  while ($c >= 0)
+  {
+    echo "A bomba irá explodir em $c <br>";
+    $c--;
+  }
+   echo "BOOM";
+
+   echo " \u{1F4A5} \u{2620} <br>";
+
+  $a= 0;
+
+  for ($a = 0; $a < 100; $a++)
+  { 
+   echo "O tempo é: {$a} <br>";
+   $a++;
+   $a++;
+   $a++;
+   $a++;
+  }
+ 
+  $c= 3;
+  $i = 0;
+  while ($i <= 30)
+  {
+      $resultado = $c * $i;
+      echo "O número {$c} multiplicado por {$i} resulta em: {$resultado} <br>";
+      $i++; 
+  }
+  
