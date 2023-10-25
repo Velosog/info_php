523cb704af0f9fa170efe675e10dd22ed2ac925c
Velosog <gabrielpveloso@gmail.com>
2023-10-24 21:42:10 -0300

    pessoa.php não completo

---
+++ pessoa.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+Criar um objeto (classe) pessoa, que comptemple os dois tipos de pessoa (jurídica e física)
+CNPJ e CPF, separando suas características/ indvidualidades conforme o tipo 
+
+1- pessoa física: CPF, Pais, Amigos, Data de Nascimento,
+2- pessoa jurídica: CNPJ, Sócios, Funcionários, Data de Fundação,
+3- Idem: endereço, idade, nome, */
+
